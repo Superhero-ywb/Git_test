a77e2ba4ad5789ef50b2443f0aa816065344ba55
Superhero-ywb <3068886705@qq.com>
2025-12-09 16:22:08 +0800

    Create 2-9.ts

---
+++ 2-9.ts
@@ -0,0 +1,37 @@
+class People{
+    name:string
+    age:number
+    addr:string
+    static count:number = 0
+    constructor(_name:string,_age:number,_addr:string,){
+        this.name = _name
+        this.addr = _addr
+        this.age = _age
+    }
+    doEat(who:string,where:string){
+        console.log(`who:${who},where:${where}`)
+    }
+    doStep(){}
+}
+class StringUtil{
+    static trimSpace(str:string){
+        return str.replace(/\s+/g,"")
+    }
+}
+
+const dataprop1 = Object.getOwnPropertyDescriptor(People.prototype,"doEat")
+const targetMethod = dataprop1!.value
+dataprop1!.value = function (...args: any[]){
+    args = args.map((arg) => {
+        if (typeof arg === "string") return StringUtil.trimSpace(arg)
+            return arg
+    })
+    console.log("前置拦截...")
+    targetMethod.apply(this,args)
+    console.log("后置拦截")
+}
+Object.defineProperty(People.prototype,"doEat",dataprop1!)
+let p = new People("peter",23,"发货的")
+p.doEat("张三","杨万宝")
+
+export{}
